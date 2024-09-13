f598dab6ada483f8a73a53997cd2eeefe2377ff9
jhyago <jaquesoantunes@gmail.com>
2024-09-12 22:12:33 -0300

    lesson26

--- 2-html-css-js/4-js-basic/calculadora/script.js
+++ 2-html-css-js/4-js-basic/calculadora/script.js
@@ -1,43 +1,43 @@
-function inserir(valor) {
-    const tela = document.formulario.tela;
+function inserir(valor){
+    let tela = document.formulario.tela
     
-    if (tela.value.length < 14) {
-        if (isOperador(valor) && isOperador(tela.value[tela.value.length - 1])) {
-            return;
+    if(tela.value.length < 14){
+        if(isOperador(valor) && isOperador(tela.value[tela.value.length - 1])){
+            return
         }
-
-        tela.value += valor;
     }
+
+    tela.value += valor
 }
 
-function limparTela() {
-    document.formulario.tela.value = ''; 
+function limparTela(){
+    document.formulario.tela.value = ""
 }
 
-function deletar() {
-    const tela = document.formulario.tela;
-    tela.value = tela.value.slice(0, -1); 
+function deletar(){
+    let tela = document.formulario.tela
+    tela.value = tela.value.slice(0, -1)
 }
 
-function calcularTotal() {
-    const tela = document.formulario.tela;
+function calcularTotal(){
+    let tela = document.formulario.tela
     const expressao = tela.value;
 
-    if (expressao && !isOperador(expressao[expressao.length - 1])) {
+    if(expressao && !isOperador(expressao[expressao.length - 1])){
         try {
-            const resultado = calcularExpressao(expressao);
-            tela.value = resultado;
-        } catch (error) {
-            alert('Expressão inválida');
-            limparTela(); 
+            let resultado = calcularExpressao(expressao) 
+            tela.value = resultado
+        } catch (error){
+            alert("Expressão inválida")
+            limparTela()
         }
     }
 }
 
-function isOperador(char) {
-    return ['+', '-', '*', '/'].includes(char);
+function isOperador (char){
+    return ['+', '-', '/', '*'].includes(char)
 }
 
-function calcularExpressao(expressao) {
-    return new Function('return ' + expressao)();
+function calcularExpressao(expressao){
+    return eval(expressao)
 }
