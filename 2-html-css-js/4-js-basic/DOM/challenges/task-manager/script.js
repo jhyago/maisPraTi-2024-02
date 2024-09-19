a17e9ef7bf5c3aeb4576b599d3f8f5c597d96200
jhyago <jaquesoantunes@gmail.com>
2024-09-19 19:57:54 -0300

    task of the day

---
+++ 2-html-css-js/4-js-basic/DOM/challenges/task-manager/script.js
@@ -0,0 +1,73 @@
+let taskList   = document.getElementById('task-list')
+let taskInput  = document.getElementById('task-input')
+let addTaskBtn = document.getElementById('add-task-btn')
+
+let filters = document.querySelectorAll('.filter-btn')
+
+addTaskBtn.addEventListener('click', () =>{
+    let taskText = taskInput.value.trim()
+
+    if(taskText) {
+        let li = createTaskElement(taskText)
+
+        taskList.appendChild(li)
+
+        taskInput.value = ''
+    }
+})
+
+function createTaskElement(text) {
+    let li = document.createElement('li')
+
+    li.textContext = text
+
+    let removeBtn = document.createElement('span')
+    removeBtn.textContent = 'Remover'
+    removeBtn.classList.add('remove-btn')
+
+    removeBtn.addEventListener('click', () => li.remove())
+
+    li.appendChild(removeBtn)
+
+    let editBtn = document.createElement('span')
+    editBtn.textContent = 'Editar'
+    editBtn.classList.add('edit-btn')
+
+    editBtn.addEventListener('click', () => editTask(li))
+
+    li.appendChild(editBtn)
+
+    li.addEventListener('click', () => {
+        if(!li.classList.contains('editing')){
+            li.classList.toggle('completed')
+        }
+    })
+
+    return li
+}
+
+function editTask(li) {
+    if(!li.classList.contains('editing')){
+        li.classList.add('editing')
+
+        let input = document.createElement('input')
+        input.type = 'text'
+        input.value = li.firstChild.textContent
+
+        li.firstChild.replaceWith(input)
+
+        input.addEventListener('blur', () => {
+            li.classList.remove('editing')
+
+            let newText = input.value.trim()
+
+            if(newText) {
+                input.replaceWith(document.createTextNode(newText))
+            } else {
+                li.remove()
+            }
+        })
+
+        input.focus()
+    }
+}
