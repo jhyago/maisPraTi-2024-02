e70c7157921ced22e43036cbf93791ded75ddb60
jhyago <jaquesoantunes@gmail.com>
2024-10-03 21:20:13 -0300

    Challenge - Search Tasks

--- 2-html-css-js/4-js-basic/4-toDoList/app.js
+++ 2-html-css-js/4-js-basic/4-toDoList/app.js
@@ -14,9 +14,8 @@ class Task {
 
                 return false
             }
-
-            return true
         }
+        return true
     }
 }
 
@@ -34,7 +33,20 @@ class Database {
         }
     }
 
-    loadTasks() {}
+    loadTasks() {
+        let tasks = []
+        let id = localStorage.getItem('id')
+
+        for(let i = 1; i <= id; i++){
+            try {
+                let task = JSON.parse(localStorage.getItem(i))
+                tasks.push(task)
+            } catch (error){
+                console.error(`Erro ao carregar a tarefa com id ${id}`)
+            }
+        }
+        return tasks
+    }
 
     createTask(task) {
         let id = this.getNextId()
@@ -42,7 +54,9 @@ class Database {
         localStorage.setItem('id', id.toString())
     }
     
-    removeTask(id){}
+    removeTask(id){
+        localStorage.removeItem(id)
+    }
 
     searchTasks(task){}
 
@@ -70,3 +84,57 @@ function registerTask(){
         alert('Preencha todos os campos.')
     }
 }
+
+function loadTasks(tasks = database.loadTasks()){
+    let listTask = document.getElementById('listTasks')
+    listTask.innerHTML = ''
+
+    tasks.forEach((task) => {
+        let row = listTask.insertRow()
+
+        row.insertCell(0).innerHTML = `${task.day}/${task.month}/${task.year}`
+
+        row.insertCell(1).innerHTML = getTaskTypeName(task.type)
+        row.insertCell(2).innerHTML = task.description
+
+        let btn = document.createElement('button')
+        btn.className = 'btn btn-danger'
+        btn.id = task.id
+        btn.innerHTML = 'Deletar'
+        btn.onClick = () => {
+            if(confirm('Você tem certeza que quer excluir está tarefa?')){
+                database.removeTask(task.id)
+                loadTasks()
+            }
+        }
+
+        row.insertCell(3).append(btn)
+    })
+}
+
+function getTaskTypeName(type){
+    switch(type) {
+        case '1':
+            return 'Studies'
+            break
+        case '2':
+            return 'Work'
+            break
+        case '3':
+            return 'Home'
+            break
+        case '4': 
+            return 'health'
+        case '5':
+            return 'Family'
+            break
+        default: 
+            return 'Desconhecido'
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    if(document.body.contains(document.getElementById('listTasks'))){
+        loadTasks()
+    }
+})
