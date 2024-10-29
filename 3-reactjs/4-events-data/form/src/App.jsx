92afde2873456369ed2727efeaec5893c6230602
jhyago <jaquesoantunes@gmail.com>
2024-10-29 19:48:57 -0300

    46

---
+++ 3-reactjs/4-events-data/form/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Form from './components/form'
+
+function App() {
+
+  return (
+    <>
+      <Form/>
+    </>
+  )
+}
+
+export default App
---
+++ 3-reactjs/4-events-data/form/src/components/form.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react'
+
+function Form() {
+    const [tarefas, setTarefas] = useState([])
+
+    const [novaTarefa, setNovaTarefa] = useState('')
+
+    const handleInputChange = (event) => {
+        setNovaTarefa(event.target.value)
+    }
+
+    const handleAddTask = () => {
+        if(novaTarefa.trim() === ''){
+            return
+        }
+
+        setTarefas([...tarefas, novaTarefa])
+        setNovaTarefa('')
+    }
+
+    const handleRemoveTask = (index) => {
+        const updatedTasks = tarefas.filter((_, i) => i !== index)
+        setTarefas(updatedTasks)
+    }
+
+    return (
+        <div>
+            <h1>Lista de Tarefas</h1>
+            <input 
+                type="text"
+                value={novaTarefa} 
+                onChange={handleInputChange}
+                placeholder='Digite uma nova tarefa'   
+            />
+            <button onClick={handleAddTask}>Adicionar Tarefa</button>
+
+            <ul>
+                {tarefas.map((tarefa, index) => (
+                    <li key={index}>
+                        {tarefa}
+                        <button onClick={() => handleRemoveTask(index)}>Remover</button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+export default Form
