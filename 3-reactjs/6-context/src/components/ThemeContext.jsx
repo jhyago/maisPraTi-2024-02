2f1370ac9828bda0c473122468f3b57421a4179c
jhyago <jaquesoantunes@gmail.com>
2024-11-02 08:45:12 -0300

    lesson48

--- 3-reactjs/6-context/src/components/ThemeContext.jsx
+++ 3-reactjs/6-context/src/components/ThemeContext.jsx
@@ -1,17 +1,25 @@
+// Importa os hooks createContext e useState do React
 import { createContext, useState } from 'react'
 
+// Cria um contexto chamado ThemeContext, que será utilizado para compartilhar o estado do tema entre componentes
 export const ThemeContext = createContext()
     
+// Define o ThemeProvider, um componente de contexto que permitirá que outros componentes acessem o tema
 // eslint-disable-next-line react/prop-types
 export const ThemeProvider = ({ children }) => {
+  
+  // Cria uma variável de estado chamada 'theme' com valor inicial "light" e uma função 'setTheme' para atualizar o valor de 'theme'
   const [theme, setTheme] = useState("light");
+
+  // Define a função 'toggleTheme', que alterna o valor de 'theme' entre "light" e "dark"
   const toggleTheme = () => {
     setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
   }
 
+  // Retorna o provedor de contexto ThemeContext.Provider com o valor atual do tema e a função toggleTheme
   return (
     <ThemeContext.Provider value={{ theme, toggleTheme }}>
-      {children}
+      {children} {/* Renderiza os componentes filhos dentro do provedor, permitindo que eles acessem o valor do contexto */}
     </ThemeContext.Provider>
   )
-}   
+}
--- 3-reactjs/6-context/src/components/ThemeSwitcher.jsx
+++ 3-reactjs/6-context/src/components/ThemeSwitcher.jsx
@@ -1,15 +1,26 @@
+// Importa o hook useContext do React para acessar o contexto
 import { useContext } from 'react'
-import { ThemeContext} from './ThemeContext'
 
+// Importa o ThemeContext, criado no arquivo ThemeContext.js
+import { ThemeContext } from './ThemeContext'
+
+// Define o componente ThemeSwitcher, que permitirá alternar o tema
 function ThemeSwitcher() {
+
+    // Usa o hook useContext para acessar o valor do ThemeContext
+    // Extraímos as variáveis 'theme' e 'toggleTheme' do contexto
     const { theme, toggleTheme } = useContext(ThemeContext)
 
+    // Renderiza o componente
     return (
         <div>
+            {/* Exibe o tema atual */}
             <p>O tema atual é: {theme}</p>
+            {/* Botão que chama a função toggleTheme ao ser clicado, alternando o tema */}
             <button onClick={toggleTheme}>Trocar o tema</button>
         </div>
     )
 }
 
+// Exporta o componente ThemeSwitcher como padrão para que possa ser usado em outros lugares do aplicativo
 export default ThemeSwitcher
