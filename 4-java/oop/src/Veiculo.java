817c460159e82d120d290e769c9e735d847617e9
jhyago <jaquesoantunes@gmail.com>
2024-12-05 19:54:09 -0300

    lesson65

---
+++ 4-java/oop/src/Veiculo.java
@@ -0,0 +1,13 @@
+//Crie um sistema de uma loja que vende produtos:
+
+//Classe Produto:
+//Atributos: nome, preco.
+//        Método: calcularPrecoFinal(), que retorna o preço sem alterações.
+//Subclasse ProdutoPerecivel:
+//Atributos adicionais: dataDeValidade.
+//Sobrescreva o método calcularPrecoFinal() para aplicar um desconto de 10% caso o produto esteja próximo da data de validade.
+//Subclasse ProdutoEletronico:
+//Atributos adicionais: garantiaEmMeses.
+//Sobrescreva o método calcularPrecoFinal() para incluir uma taxa de garantia de 5% no preço.
+
+//Implemente as classes e crie uma lista de produtos com diferentes tipos. Mostre o preço final de cada produto usando o polimorfismo.
