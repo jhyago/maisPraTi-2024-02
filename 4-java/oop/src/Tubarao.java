a015b279779545c86aabc1fa058369338a30e33b
jhyago <jaquesoantunes@gmail.com>
2024-12-04 21:23:05 -0300

    lesson63

---
+++ 4-java/oop/src/Tubarao.java
@@ -0,0 +1,15 @@
+//Sistema de Pagamentos
+
+//Implemente um sistema de pagamentos com:
+
+//Uma classe base Pagamento com um método realizarPagamento()
+
+//Subclasses:
+
+//PagamentoCredito: Aplica uma taxa de 2%.
+//PagamentoDebito: Aplica uma taxa de 1%.
+//PagamentoPix: Não aplica taxas.
+
+//Use sobrecarga em PagamentoPix para um método que permite adicionar uma descrição da transação.
+
+//No programa principal, processe pagamentos de diferentes formas e demonstre o uso de polimorfismo e sobrecarga.
