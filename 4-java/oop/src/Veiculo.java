1a1309561c10c1aa24a998892e1f0384237a5fc7
jhyago <jaquesoantunes@gmail.com>
2024-12-05 20:16:55 -0300

    lesson65

--- 4-java/oop/src/Veiculo.java
+++ 4-java/oop/src/Veiculo.java
@@ -5,9 +5,22 @@
 //        Método: calcularPrecoFinal(), que retorna o preço sem alterações.
 //Subclasse ProdutoPerecivel:
 //Atributos adicionais: dataDeValidade.
-//Sobrescreva o método calcularPrecoFinal() para aplicar um desconto de 10% caso o produto esteja próximo da data de validade.
+//Sobrescreva o método calcularPrecoFinal() para aplicar um desconto de 10% caso o produto esteja próximo
+//da data de validade.
 //Subclasse ProdutoEletronico:
 //Atributos adicionais: garantiaEmMeses.
 //Sobrescreva o método calcularPrecoFinal() para incluir uma taxa de garantia de 5% no preço.
 
-//Implemente as classes e crie uma lista de produtos com diferentes tipos. Mostre o preço final de cada produto usando o polimorfismo.
+//Implemente as classes e crie uma lista de produtos com diferentes tipos. Mostre o preço final de
+// cada produto usando o polimorfismo.
+
+//Modele um sistema de gestão de projetos com as seguintes classes:
+
+//Classe Projeto:
+//Atributos: nome, membros (uma lista de objetos do tipo Pessoa).
+//Métodos: adicionarMembro() e listarMembros().
+//Classe Pessoa:
+//Atributos: nome, projetos (uma lista de objetos do tipo Projeto).
+//Métodos: adicionarProjeto().
+
+//Garanta que, ao adicionar um membro a um projeto, o projeto seja automaticamente associado ao membro e vice-versa.
