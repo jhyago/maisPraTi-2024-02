5fb384515cc1a208f1769b4308b7075ac193b2d2
jhyago <jaquesoantunes@gmail.com>
2025-01-13 21:37:41 -0300

    Aula 77

--- 6-spring/api-user/src/main/java/com/example/api_user/model/Usuario.java
+++ 6-spring/api-user/src/main/java/com/example/api_user/model/Usuario.java
@@ -3,11 +3,39 @@ package com.example.api_user.model;
 
 import lombok.*;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class Usuario {
     private Long id;
     private String nome;
+
+    public Usuario(Long id, String nome, String email) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     private String email;
 }
