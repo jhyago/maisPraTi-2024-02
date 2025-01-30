f8dc0abcdd06117bbfba8324f3101eb023c6cab3
jhyago <jaquesoantunes@gmail.com>
2025-01-30 19:42:05 -0300

    Aula 87

---
+++ 6-spring/jwt-auth/src/main/java/com/example/jwt_auth/config/SwaggerConfig.java
@@ -0,0 +1,29 @@
+package com.example.jwt_auth.config;
+
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API de autenticação com JWT")
+                        .version("1.0")
+                        .description("API para autenticação.")
+                        .license(new License().name("Apache 2.0").url("https://springdoc.org.com")));
+    }
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public")
+                .pathsToMatch("/auth/**", "/users/**")
+                .build();
+    }
+}
--- 6-spring/jwt-auth/src/main/java/com/example/jwt_auth/controller/AuthController.java
+++ 6-spring/jwt-auth/src/main/java/com/example/jwt_auth/controller/AuthController.java
@@ -9,6 +9,8 @@ import com.example.jwt_auth.model.User; // Representa o modelo de usuário.
 import com.example.jwt_auth.service.RefreshTokenService;
 import com.example.jwt_auth.service.UserService; // Serviço para lógica relacionada a usuários.
 import com.example.jwt_auth.util.JwtUtil; // Classe utilitária para manipulação de tokens JWT.
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.data.domain.Page;
 import org.springframework.beans.factory.annotation.Autowired; // Permite injeção de dependência automática.
 import org.springframework.data.domain.PageRequest;
@@ -31,6 +33,7 @@ import java.util.Map;
 // Marca esta classe como um controlador REST, gerenciando endpoints relacionados à autenticação.
 @RestController
 @RequestMapping("/auth") // Define o prefixo de URL para todos os endpoints deste controlador.
+@Tag(name = "Autenticação", description="Endpoints para Autenticação")
 public class AuthController {
 
     // Injeta o gerenciador de autenticação do Spring Security.
@@ -54,6 +57,7 @@ public class AuthController {
 
     // Endpoint para registrar um novo usuário.
     @PostMapping("/register") // Define o método HTTP POST para a URL "/auth/register".
+    @Operation(summary = "Registrar um novo usuário")
     public ResponseEntity<String> registerUser(@RequestBody User user) {
         // Validação: Verifica se o nome de usuário já está em uso.
         if (userService.findByUsername(user.getUsername()) != null) {
@@ -68,37 +72,37 @@ public class AuthController {
     }
 
     // Endpoint para login de usuários.
-    @PostMapping("/login") // Define o método HTTP POST para a URL "/auth/login".
-    public ResponseEntity<String> login(@RequestBody User user) {
-        try {
-            // Exibe no console os dados do usuário que está tentando se autenticar (usado para depuração).
-            System.out.println("Tentando autenticar: " + user.getUsername());
-            System.out.println("Senha fornecida: " + user.getPassword());
-
-            // Realiza a autenticação utilizando o gerenciador de autenticação do Spring Security.
-            Authentication authentication = authenticationManager.authenticate(
-                    new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword())
-            );
-
-            // Exibe no console que o usuário foi autenticado com sucesso.
-            System.out.println("Usuário autenticado com sucesso: " + authentication.getName());
-
-            // Gera um token JWT para o usuário autenticado.
-            String accessToken = jwtUtil.generateToken(authentication.getName());
-            RefreshToken refreshToken = refreshTokenService.createRefreshToken(authentication.getName());
-
-            Map<String, String> tokens = new HashMap<>();
-            tokens.put("accessToken", accessToken);
-            tokens.put("refreshToken", refreshToken.getToken());
-
-            // Retorna o token no corpo da resposta.
-            return ResponseEntity.ok(tokens);
-
-        } catch (AuthenticationException e) {
-            // Exibe o erro no console e retorna uma resposta de erro 401.
-            throw new AuthenticationException("Usuário ou senha Inválidos") {};
-            }
-    }
+//    @PostMapping("/login") // Define o método HTTP POST para a URL "/auth/login".
+//    public ResponseEntity<String> login(@RequestBody User user) {
+//        try {
+//            // Exibe no console os dados do usuário que está tentando se autenticar (usado para depuração).
+//            System.out.println("Tentando autenticar: " + user.getUsername());
+//            System.out.println("Senha fornecida: " + user.getPassword());
+//
+//            // Realiza a autenticação utilizando o gerenciador de autenticação do Spring Security.
+//            Authentication authentication = authenticationManager.authenticate(
+//                    new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword())
+//            );
+//
+//            // Exibe no console que o usuário foi autenticado com sucesso.
+//            System.out.println("Usuário autenticado com sucesso: " + authentication.getName());
+//
+//            // Gera um token JWT para o usuário autenticado.
+//            String accessToken = jwtUtil.generateToken(authentication.getName());
+//            RefreshToken refreshToken = refreshTokenService.createRefreshToken(authentication.getName());
+//
+//            Map<String, String> tokens = new HashMap<>();
+//            tokens.put("accessToken", accessToken);
+//            tokens.put("refreshToken", refreshToken.getToken());
+//
+//            // Retorna o token no corpo da resposta.
+//            return ResponseEntity.ok(tokens);
+//
+//        } catch (AuthenticationException e) {
+//            // Exibe o erro no console e retorna uma resposta de erro 401.
+//            throw new AuthenticationException("Usuário ou senha Inválidos") {};
+//            }
+//    }
 
     @PostMapping("/refresh-token")
     public ResponseEntity<String> refreshToken(@RequestBody RefreshTokenDTO refreshToken) {
